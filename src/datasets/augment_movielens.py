7977b2cd87f32e2b216865770f055d0672664067
Maxim Faleev <m.faleev@innopolis.university>
2022-05-09 13:39:59 +0300

    + augmentation with genres bitmask

--- src/datasets/augment_movielens.py
+++ src/datasets/augment_movielens.py
@@ -1,3 +1,4 @@
+import itertools as it
 import pandas as pd
 import numpy as np
 
@@ -15,8 +16,24 @@ def augment_with_ratings(movielens: pd.DataFrame) -> pd.DataFrame:
     }, axis=1, inplace=True)
     return movielens.join(movielens_ratings, on='Title')
 
+
+def augment_with_genres(movielens: pd.DataFrame) -> pd.DataFrame:
+    genres = set(it.chain(*movielens.Genres.apply(lambda x: x.split("|"))))
+    genre_columns = [f"is{genre}Movie" for genre in genres]
+    genre_bitmasks = list(movielens.Genres.apply(encode_ohe_genre, args=[genres]))
+    movielens[genre_columns] = genre_bitmasks
+    del movielens['Genres']
+    return movielens
+
+def encode_ohe_genre(genre_str: str, all_genres: set) -> list:
+    title_genres = set(genre_str.split("|"))
+    bitmask_genres = [genre in title_genres for genre in all_genres]
+    return bitmask_genres
+
+
 if __name__ == '__main__':
     movielens = pd.read_csv("data/movie_lens/movie_lens_1m.csv")
     augmented_movielens = augment_with_ratings(movielens)
+    augmented_movielens = augment_with_genres(augmented_movielens)
     augmented_movielens.to_csv("data/augmented_movie_lens.csv", index=False)
 
