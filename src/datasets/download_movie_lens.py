f61ce372ca682e286c4799174c8f478679f8f528
Rizvan Iskaliev <rizvaninno@gmail.com>
2022-05-15 20:34:41 +0300

    Script with MovieLens dataset downloading is refactored.

--- src/datasets/download_movie_lens.py
+++ src/datasets/download_movie_lens.py
@@ -3,6 +3,25 @@ import os
 import logging
 from argparse import ArgumentParser
 from zipfile import ZipFile
+from typing import Optional
+
+
+def load_movie_lens(
+    url: Optional[str] = "https://files.grouplens.org/datasets/movielens/ml-1m.zip",
+    save_dir: Optional[str] = "./data/movie_lens",
+    name: Optional[str] = "movie_lens_1m.zip"
+) -> None:
+    os.makedirs(save_dir, exist_ok=True)
+    logging.info("Downloading file...")
+    wget.download(url=url, out=os.path.join(save_dir, name))
+    print("\n")
+    logging.info("File is downloaded, unzipping...")
+    with ZipFile(os.path.join(save_dir, name), "r") as zip_f:
+        zip_f.extractall(path=save_dir)
+    logging.info("Data is unzipped.")
+
+    os.remove(os.path.join(save_dir, name))
+    logging.info(".zip file is deleted.")
 
 
 if __name__ == "__main__":
@@ -26,14 +45,4 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
 
-    os.makedirs(args.save_dir, exist_ok=True)
-    logging.info("Downloading file...")
-    wget.download(url=args.url, out=os.path.join(args.save_dir, args.name))
-    print("\n")
-    logging.info("File is downloaded, unzipping...")
-    with ZipFile(os.path.join(args.save_dir, args.name), "r") as zip_f:
-        zip_f.extractall(path=args.save_dir)
-    logging.info("Data is unzipped.")
-
-    os.remove(os.path.join(args.save_dir, args.name))
-    logging.info(".zip file is deleted.")
+    load_movie_lens(args.url, args.save_dir, args.name)
